99366ff4cfabbfbc85cc52888770e293150fcd5c
FathurAnshari <anshariF17@gmail.com>
2022-06-16 11:22:53 +0700

    logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
@@ -58,6 +59,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }
